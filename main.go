678b43a0737c75443595bbeaa70f02363722167f
Tamal Saha <tamal@appscode.com>
2022-11-16 11:52:52 +0600

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -41,31 +41,35 @@ func main() {
 	}
 
 	target, _ := url.Parse(api.BaseURL)
-	targetQuery := target.RawQuery
-	director := func(req *http.Request) {
-		req.URL.Scheme = target.Scheme
-		req.URL.Host = target.Host
-		req.URL.Path, req.URL.RawPath = joinURLPath(target, req.URL)
-		if targetQuery == "" || req.URL.RawQuery == "" {
-			req.URL.RawQuery = targetQuery + req.URL.RawQuery
-		} else {
-			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
-		}
-		if _, ok := req.Header["User-Agent"]; !ok {
-			// explicitly disable User-Agent so it's not set to default value
-			req.Header.Set("User-Agent", "")
-		}
+	//targetQuery := target.RawQuery
+	//director := func(req *http.Request) {
+	//	req.URL.Scheme = target.Scheme
+	//	req.URL.Host = target.Host
+	//	req.URL.Path, req.URL.RawPath = joinURLPath(target, req.URL)
+	//	if targetQuery == "" || req.URL.RawQuery == "" {
+	//		req.URL.RawQuery = targetQuery + req.URL.RawQuery
+	//	} else {
+	//		req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+	//	}
+	//	if _, ok := req.Header["User-Agent"]; !ok {
+	//		// explicitly disable User-Agent so it's not set to default value
+	//		req.Header.Set("User-Agent", "")
+	//	}
+	//	req.Header.Set("Authorization", "Bearer "+api.APIToken)
+	//	// Accept-Encoding: gzip
+	//	// req.Header.Set("Accept-Encoding", "gzip")
+	//
+	//	req.Host = ""
+	//}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	d := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		d(req)
+		req.Host = ""
 		req.Header.Set("Authorization", "Bearer "+api.APIToken)
-		// Accept-Encoding: gzip
-		req.Header.Set("Accept-Encoding", "gzip")
-
-		req.Host = "api.cloudflare.com"
 	}
-
-	frontendProxy := httptest.NewServer(&httputil.ReverseProxy{
-		Director:  director,
-		Transport: auther{},
-	})
+	frontendProxy := httptest.NewServer(proxy)
 	defer frontendProxy.Close()
 
 	fmt.Println(frontendProxy.URL)
